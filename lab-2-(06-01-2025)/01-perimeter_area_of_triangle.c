ccd200d33ef35abb91bf934ee43a527a5c43829d
Asem Hamid <197576625+as-em@users.noreply.github.com>
2025-02-07 22:06:07 +0600

    Create 01-perimeter_area_of_triangle.c

---
+++ lab-2-(06-01-2025)/01-perimeter_area_of_triangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    float a , b ,c ;
+    float perimeter, half_perimeter , area;
+
+    printf("Please input 3 are of triangle :");
+    scanf("%f %f %f",&a ,&b ,&c);
+
+    perimeter = a + b + c;
+    printf("Perimeter of triangle: %.2f \n",perimeter);
+
+    half_perimeter = perimeter / 2;
+
+    area = sqrt(half_perimeter * (half_perimeter - a) * (half_perimeter - b) * (half_perimeter - c ) );
+    printf("Area of triangle: %.2f \n",area);
+
+
+    return 0;
+}
+
