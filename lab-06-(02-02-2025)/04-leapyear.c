8f611afa14446215593215f458075ece5fd133ad
Asem Hamid <197576625+as-em@users.noreply.github.com>
2025-02-23 23:03:43 +0600

    Update 04-leapyear.c

--- lab-06-(02-02-2025)/04-leapyear.c
+++ lab-06-(02-02-2025)/04-leapyear.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
-int main (){
 
+int main() {
     int year;
-    printf("Enter year : \n");
+
+
+    printf("Enter a year: ");
     scanf("%d", &year);
 
-    if( year%400==0 || (year%4==0 && year!=100)){
-        printf("Leap year.");
-    }
+   
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+
+        printf("%d is a leap year.\n", year);
+
+    } 
     else {
-        printf("Not a leap year.");
+
+        printf("%d is not a leap year.\n", year);
+        
     }
+
     return 0;
 }
