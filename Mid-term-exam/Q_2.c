fa5c425680a54f1df670b2adb998bbd3d59e8aa0
Asem Hamid <197576625+as-em@users.noreply.github.com>
2025-02-23 22:17:45 +0600

    Create Q_2.c

---
+++ Mid-term-exam/Q_2.c
@@ -0,0 +1,18 @@
+// 2. Write a C program to find the area and perimeter of a rectangle.
+
+#include <stdio.h>
+
+int main(){
+
+    float l, w, perimeter, area;
+
+    printf("Enter length and width: ");
+    scanf("%f %f", &l, &w);
+
+    perimeter = 2 * (l + w);
+    area = l * w;
+
+    printf("The area of rectangle is : %.2f and perimeter is : %.2f", area, perimeter);
+
+    return 0;
+}
