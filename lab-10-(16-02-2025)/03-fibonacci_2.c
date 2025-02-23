be0c28c837df01fc4af547f62833a90aca7049c2
Asem Hamid <197576625+as-em@users.noreply.github.com>
2025-02-24 03:02:02 +0600

    Update 03-fibonacci_2.c

--- lab-10-(16-02-2025)/03-fibonacci_2.c
+++ lab-10-(16-02-2025)/03-fibonacci_2.c
@@ -4,11 +4,11 @@ int main()
     int n , x=0 , y=1 , i , s;
     printf("N = ");
     scanf("%d", &n);
-    printf("%d %d", x , y);
-    for(i=2; i<=n; i++)
+    printf("%d %d ", x , y);
+    for(i=2; i< n; i++)
     {
         s = x+y;
-        printf("%d", s );
+        printf("%d ", s );
         x = y;
         y = s;
     }
