b5ab24c1ce10582cbaf903fc9000af85224905bb
Asem Hamid <197576625+as-em@users.noreply.github.com>
2025-02-24 00:50:15 +0600

    Create Q_16.c

---
+++ Mid-term-exam/Q_16.c
@@ -0,0 +1,22 @@
+// 16. Write a C program to find the summation of digits of a given number. 
+
+#include <stdio.h>
+
+int main(){
+
+    int n, sum=0;
+
+    printf("Enter a digit : ");
+    scanf("%d", &n);
+
+    for(int i=1; n!=0; i++){
+
+        sum += n%10; 
+        n = n/10;
+
+    }
+
+    printf("Sum of digits: %d\n", sum);
+
+    return 0;
+}
