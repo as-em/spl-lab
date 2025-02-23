c5a5bcbea262245ab05c5241f361437534bcc873
Asem Hamid <197576625+as-em@users.noreply.github.com>
2025-02-23 23:09:04 +0600

    Create Q_9.c

---
+++ Mid-term-exam/Q_9.c
@@ -0,0 +1,27 @@
+// 9. Write a C program to convert the given temperature in Fahrenheit to Celsius. 
+
+#include <stdio.h>
+
+int main() {
+
+   float f1 , c1 , f2 , c2;
+
+    printf("Enter the temperature in Fahrenheit: ");
+    scanf("%f", &f1);
+
+    c1 = ( (f1 - 32) * 5) / 9;
+    // c1 = ( ( f1 - 32) / 9) * 5;
+    printf("%.3f Fahrenheit = %.3f Celsius \n", f1, c1);
+
+    printf("Enter the temperature in Celsius: ");
+    scanf("%f", &c2);
+
+
+    f2 = ( ( c2 / 5 ) * 9 ) + 32;
+    // f2 = ( ( c2 * 9 ) / 5 ) + 32
+    printf("%.3f Celsius = %.3f Fahrenheit \n", c2, f2);
+
+
+
+    return 0;
+}
