099a9ed43b57815a64275b8aa0521686409a85b1
Asem Hamid <197576625+as-em@users.noreply.github.com>
2025-02-26 12:06:34 +0600

    Update Q_05_2.c

--- Mid-term-exam/Q_05_2.c
+++ Mid-term-exam/Q_05_2.c
@@ -1,7 +1,7 @@
 // 5. Write a C program to find if a triangle is a Right angled triangle or not.
 
 #include<stdio.h>
-#include<math.h>
+
 
 int main(){
 
@@ -9,7 +9,7 @@ int main(){
     printf("three num :");
     scanf("%d%d%d",&a,&b,&c);
 
-    if(c == sqrt((a*a)+(b*b)) || b == sqrt((a*a)+(c*c)) || a == sqrt((c*c)+(b*b)) ){
+    if(c*c == ((a*a)+(b*b)) || b*b == ((a*a)+(c*c)) || a*a == ((c*c)+(b*b)) ){
         printf("right");
     }
     else{
