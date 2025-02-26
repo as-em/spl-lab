b68cd6454754f1e3baab6b5401242d179aeb2ab1
Asem Hamid <197576625+as-em@users.noreply.github.com>
2025-02-26 11:52:36 +0600

    Update Q_17.c

--- Mid-term-exam/Q_17.c
+++ Mid-term-exam/Q_17.c
@@ -1,18 +1,32 @@
 // 17. Write a C program to find the print first n Fibonacci Number. 
 
-#include<stdio.h>
-int main()
-{
-    int n , x=0 , y=1 , i , s;
-    printf("N = ");
-    scanf("%d", &n);
-    printf("%d %d ", x , y);
-    for(i=2; i< n; i++)
-    {
-        s = x+y;
-        printf("%d ", s );
-        x = y;
-        y = s;
-    }
-    return 0;
+
+#include <stdio.h>
+
+
+
+int main (){
+
+   int n,  i ;
+   int x ,  y ,  s ;
+   
+   printf("Enter n : ");
+   scanf("%d", &n);
+   
+   x = 0 ;
+   y = 1 ;
+   printf("%d %d ", x, y);
+ 
+   
+   for( i = 2 ; i <= n;i++){
+       
+       s =   x + y ;
+       x =  y;
+       y =  s;
+       
+       printf("%d ", s );
+       
+   }
+    
+return 0;
 }
