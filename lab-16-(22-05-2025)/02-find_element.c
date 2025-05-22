cb2e2fd03b8b36e7045fba9fe7957775c9349770
Asem Hamid <197576625+as-em@users.noreply.github.com>
2025-05-22 15:33:02 +0600

    Create 02-find_element.c

---
+++ lab-16-(22-05-2025)/02-find_element.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+int arr[10],i;
+for ( i=0; i<10; i++){
+        printf("Enter Element[%d]: ", i+1);
+scanf("%d",&arr[i]);
+}
+printf("enter the element ypu want to find: ");
+int f, n=0;
+scanf("%d", &f);
+for( i=0; i<10; i++){
+    if(f == arr[i]){
+        printf("array index: %d", i);
+        return 0;
+    }
+
+    }
+    printf("not found");
+    return 0;
+}
+
+
+
